8172f03b8e040f83a00ce565d4f7894bbedfc80b
irieryota <ryotairie123@gmail.com>
2020-07-03 03:04:12 +0900

    feat(*): leader election

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ enum NodeState {
     Follower,
     Candidate,
 }
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 enum NodeActivity {
     Active,
     Stopped,
@@ -116,20 +116,24 @@ enum Request {
     AppendEntries(AppendEntriesReq),
 }
 
-struct Event {
-    node: NodeId,
-    time: i64,
-    event_type: EventType,
+enum Event {
+    Stop(NodeId, i64),
+    Resume(NodeId, i64),
+    Request(i64, i64),
 }
 
-enum EventType {
-    Stop,
-    Resume,
-    Request(i64),
+impl Event {
+    fn time(&self) -> &i64 {
+        match self {
+            Event::Stop(_, time) => time,
+            Event::Resume(_, time) => time,
+            Event::Request(_, time) => time,
+        }
+    }
 }
 
 impl Node {
-    fn new(
+    pub fn new(
         id: NodeId,
         default_append_entry_due: i64,
         default_next_election_due: i64,
@@ -153,7 +157,12 @@ impl Node {
     }
 
     fn last_log_index(&self) -> i64 {
-        (self.logs.len() - 1) as i64
+        let len = self.logs.len() as i64;
+        if len == 0 {
+            0
+        } else {
+            len - 1
+        }
     }
 
     fn last_log_term(&self) -> i64 {
@@ -165,7 +174,15 @@ impl Node {
         }
     }
 
+    fn stopped(&self) -> bool {
+        self.activity == NodeActivity::Stopped
+    }
+
     fn request_vote(&mut self, node_ids: &[NodeId]) -> Vec<Request> {
+        if self.stopped() {
+            return vec![];
+        }
+
         self.current_term = Term(self.current_term.0 + 1);
         self.voted_for = Some(self.id.clone());
         self.next_election_due = Some(self.default_next_election_due);
@@ -189,36 +206,83 @@ impl Node {
     }
 
     // リーダーとフォロワーでログの不整合が発生した場合,不整合が発生しなくなるところまでリーダーは配信するログを巻き戻す(nextIndexをdecrementする)
-    fn append_entry(&self) -> Request {
-        unimplemented!()
+    fn append_entry(&self, node_ids: &[NodeId]) -> Vec<Request> {
+        if self.stopped() {
+            return vec![];
+        }
+
+        node_ids
+            .iter()
+            .filter_map(|id| {
+                if id == &self.id {
+                    None
+                } else {
+                    Some(Request::AppendEntries(AppendEntriesReq {
+                        term: self.current_term.clone(),
+                        from: self.id.clone(),
+                        to: id.clone(),
+                        prev_index: 0, //　ちゃんと埋める
+                        prev_term: 0,  //ちゃんと埋める
+                        commit_index: 0,
+                        entries: vec![], //ちゃんと埋める
+                        time_to_take: self.default_request_time_to_take,
+                    }))
+                }
+            })
+            .collect()
     }
 
-    fn apply_request(&mut self, req: Request) -> Reply {
+    fn apply_request(&mut self, req: Request) -> Option<Reply> {
+        if self.stopped() {
+            return None;
+        }
         match req {
             Request::RequestVote(body) => {
                 if body.term > self.current_term {
-                    self.current_term = body.term.clone()
+                    self.current_term = body.term.clone();
+                    self.voted_for = None
                 }
                 let last_log_term = self.last_log_term();
                 let last_log_index = self.last_log_index();
                 // 誰にも投票していないか自分に投票していて,なおかつ保持している(commitされている,ではない!)ログが自分の最新のものよりtermが同等か新しいかつindexが同等か新しければok
                 let granted = body.term >= self.current_term
-                    && (self.voted_for == None || self.voted_for == Some(self.id.clone()))
+                    && (self.voted_for == None || self.voted_for == Some(body.from.clone()))
                     && body.last_log_index >= last_log_index
                     && body.last_log_term >= last_log_term;
-                Reply::RequestVote(RequestVoteResp {
+                if granted {
+                    self.voted_for = Some(body.from.clone());
+                    self.state = NodeState::Follower;
+                    self.peers = HashMap::new();
+                }
+                Some(Reply::RequestVote(RequestVoteResp {
                     from: self.id.clone(),
                     to: body.from,
                     time_to_take: self.default_request_time_to_take,
                     term: self.current_term.clone(),
                     granted: granted,
-                })
+                }))
             }
-            Request::AppendEntries(body) => unimplemented!(),
+            Request::AppendEntries(body) => {
+                let success = body.term >= self.current_term;
+                let match_index = 0;
+                self.next_election_due = Some(self.default_next_election_due);
+                Some(Reply::AppendEntries(AppendEntriesResp {
+                    term: self.current_term.clone(),
+                    from: self.id.clone(),
+                    to: body.from,
+                    success: success,
+                    match_index: match_index,
+                    time_to_take: self.default_request_time_to_take,
+                }))
+            }
         }
     }
 
-    fn apply_reply(&mut self, rep: Reply) {
+    fn apply_reply(&mut self, node_ids: &[NodeId], rep: Reply) {
+        if self.stopped() {
+            return;
+        }
+
         match rep {
             Reply::RequestVote(body) => {
                 if body.granted {
@@ -243,20 +307,35 @@ impl Node {
                             |sum, (_, peer)| if peer.vote_granted { sum + 1 } else { sum },
                         );
                     // 過半数
-                    if voted_count > 5 / 2 {
-                        self.state = NodeState::Leader
+                    if voted_count > node_ids.len() / 2 {
+                        self.state = NodeState::Leader;
+                        self.next_append_entry_due = Some(0);
+                        self.next_election_due = None;
+                    }
+                } else {
+                    if body.term > self.current_term {
+                        self.current_term = body.term
                     }
                 }
             }
-            Reply::AppendEntries(body) => unimplemented!(),
+            Reply::AppendEntries(body) => {
+                //実装しようね
+            }
         }
     }
 }
 
 impl History {
+    fn new(events: Vec<Event>) -> History {
+        History {
+            states: vec![],
+            events: events,
+        }
+    }
+
     fn get_history(&mut self, initial_state: State, target_time: i64) {
         let mut state = initial_state;
-        for _ in 0..=target_time {
+        for _ in 0..target_time {
             state = state.next_state(&self.events);
             self.states.push(state.clone())
         }
@@ -280,13 +359,14 @@ impl State {
 
     fn next_state(self, events: &[Event]) -> Self {
         let time = self.time + 1;
-        let events: Vec<&Event> = events.iter().filter(|ev| ev.time == time).collect();
+        let state = State { time: time, ..self };
+        let events: Vec<&Event> = events.iter().filter(|ev| ev.time() == &time).collect();
         let State {
             time,
             mut nodes,
             requests,
             replies,
-        } = events.iter().fold(self, |sum, ev| sum.apply_event(ev));
+        } = events.iter().fold(state, |sum, ev| sum.apply_event(ev));
         //非効率だが読みやすさ重視
         let requests = requests.iter().map(|r| r.next_state());
         let replies = replies.iter().map(|r| r.next_state());
@@ -296,41 +376,39 @@ impl State {
         let (reached_replies, unreached_replies): (Vec<Reply>, Vec<Reply>) =
             replies.partition(|r| r.reached());
 
+        let node_ids: Vec<NodeId> = nodes.iter().map(|(id, node)| id.clone()).collect();
+
         let mut new_replies = vec![];
         for req in reached_requests {
             let node = nodes.get_mut(&req.to()).unwrap();
-            let reply = node.apply_request(req);
-            new_replies.push(reply);
+            if let Some(reply) = node.apply_request(req) {
+                new_replies.push(reply);
+            }
         }
 
         for rep in reached_replies {
             let node = nodes.get_mut(&rep.to()).unwrap();
-            node.apply_reply(rep);
+            node.apply_reply(&node_ids, rep);
         }
         let mut new_requests = vec![];
-
-        let node_ids: Vec<NodeId> = nodes.iter().map(|(id, node)| id.clone()).collect();
-
         nodes.iter_mut().for_each(|(_, node)| {
             match node.state {
-                NodeState::Leader => {
-                    match node.next_append_entry_due {
-                        Some(due) if due > 0 => node.next_append_entry_due = Some(due - 1),
-                        Some(_) => {
-                            // 期限切れの場合append_entryする
-                            //let entry = node.append_entry();
-                            //new_requests.push(entry);
-                        }
-                        None => node.next_append_entry_due = Some(node.default_append_entry_due),
+                NodeState::Leader => match node.next_append_entry_due {
+                    Some(due) if due > 0 => node.next_append_entry_due = Some(due - 1),
+                    Some(_) => {
+                        let mut requests = node.append_entry(&node_ids);
+                        new_requests.append(&mut requests);
                     }
-                }
+                    None => node.next_append_entry_due = Some(node.default_append_entry_due),
+                },
                 NodeState::Follower => match node.next_election_due {
                     None => node.next_election_due = Some(node.default_next_election_due),
                     Some(due) if due > 0 => node.next_election_due = Some(due - 1),
                     Some(_) => {
                         //立候補
                         node.state = NodeState::Candidate;
-                        node.request_vote(&node_ids);
+                        let mut requests = node.request_vote(&node_ids);
+                        new_requests.append(&mut requests)
                     }
                 },
                 NodeState::Candidate => {
@@ -340,7 +418,8 @@ impl State {
                         Some(_) => {
                             // timeoutしたらまた立候補
                             node.state = NodeState::Candidate;
-                            node.request_vote(&node_ids);
+                            let mut requests = node.request_vote(&node_ids);
+                            new_requests.append(&mut requests)
                         }
                     }
                 }
@@ -353,28 +432,31 @@ impl State {
         replies.append(&mut new_replies);
 
         State {
-            time,
-            nodes,
-            requests,
-            replies,
+            time: time,
+            nodes: nodes,
+            requests: requests,
+            replies: replies,
         }
     }
 
     fn apply_event(self, event: &Event) -> Self {
-        match event.event_type {
-            EventType::Stop => {
+        match event {
+            Event::Stop(id, _) => {
                 let mut nodes = self.nodes;
-                let mut node = nodes.get_mut(&event.node).unwrap();
+                let mut node = nodes.get_mut(id).unwrap();
                 node.activity = NodeActivity::Stopped;
                 State { nodes, ..self }
             }
-            EventType::Resume => {
+            Event::Resume(id, _) => {
                 let mut nodes = self.nodes;
-                let mut node = nodes.get_mut(&event.node).unwrap();
-                node.activity = NodeActivity::Active;
+                let mut node = nodes.get_mut(id).unwrap();
+                if node.stopped() {
+                    node.activity = NodeActivity::Active;
+                    node.state = NodeState::Follower
+                }
                 State { nodes, ..self }
             }
-            EventType::Request(val) => self,
+            Event::Request(val, _) => self,
         }
     }
 }
@@ -438,5 +520,37 @@ impl Reply {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let nodes = vec![
+        Node::new(NodeId(1), 1, 2, 1),
+        Node::new(NodeId(2), 1, 3, 1),
+        Node::new(NodeId(3), 1, 10, 1),
+        Node::new(NodeId(4), 1, 7, 1),
+        Node::new(NodeId(5), 1, 9, 1),
+    ];
+    let initial_state = State::new(nodes);
+    let events = vec![Event::Stop(NodeId(1), 6)];
+    let mut history = History::new(events);
+    history.get_history(initial_state, 20);
+    for state in history.states {
+        println!(
+            "time: {:?}\nnodes1: {:?}\nnode2: {:?}\nnode3: {:?}\nnode4: {:?}\nnode5: {:?}\nrequests: {:?}\nreplues: {:?}\n",
+            state.time,
+            state.nodes[&NodeId(1)],
+            state.nodes[&NodeId(2)],
+            state.nodes[&NodeId(3)],
+            state.nodes[&NodeId(4)],
+            state.nodes[&NodeId(5)],
+            state.requests,
+            state.replies,
+        );
+    }
+}
+
+#[test]
+fn leader_election() {
+    let nodes = vec![];
+    let initial_state = State::new(nodes);
+    let mut history = History::new();
+    history.get_history(initial_state, 100);
+    println!("{:?}", history.states);
 }
